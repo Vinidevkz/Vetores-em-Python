528571b4fdfc4c7f69014db39fdeae73479d75da
Vinicius Eduardo <149980701+Vinidevkz@users.noreply.github.com>
2024-08-02 17:12:57 -0300

    First Commit

---
+++ Vetores Python/Listas Ordenadas.py
@@ -0,0 +1,58 @@
+#Deletar Índices
+"""listanumeros = [100, 200, 300, 400, 500]
+print(listanumeros)
+
+listanumeros.pop()
+print(listanumeros)
+
+listanumeros.pop(-4)
+print(listanumeros)"""
+
+#Saber o índice de um objeto
+"""listadesenhos= ['horaDeAventura', 'apenasUmShow', 'gravityFalls']
+print(listadesenhos.index('gravityFalls')) #gravityFalls é o 2"""
+
+#Operadores "in" para saber se o índice existe ou não
+"""listatimes = ['Corinthians', 'São Paulo', 'Santos', 'Palmeiras']
+print(listatimes)
+
+#Portuguesa não existe
+print('Portuguesa' in listatimes)
+#Palmeiras existe
+print('Palmeiras' in listatimes)"""
+
+#Concatenar Listas
+"""lista1 = [1, 2, 3]
+lista2 = [4, 5, 6]
+listaTotal = lista1 + lista2
+
+print(listaTotal)"""
+
+#Repetição de lista
+"""lista = [1, 2, 3]
+listaRepeticao = lista * 3
+print(listaRepeticao)"""
+
+#Função SUM com resultado 17
+"""lista = [2, 4, 5, 6]
+print(sum(lista))"""
+
+#Encontrar o menor e o maior valor de uma lista
+"""lista = [10,20,30,40,50,60,70,80,90,100]
+
+print(min(lista))
+print((max(lista)))"""
+
+#Conta o total de elementos em uma lista
+"""lista = [10, 20, 30, 40, 50]
+print(len(lista))"""
+
+#Pegar o item pelo índice
+"""lista = [10,20,30,40,50,60,70,80,90,100]
+
+print('Primeiro item da lista: ', lista[0])
+print('Primeiro item da lista: ', lista[-1])"""
+
+
+
+
---
+++ Vetores Python/Vetores Python.py
@@ -0,0 +1,19 @@
+#lista1 = [1, 2, 3]
+#print(lista1)
+
+lista1 = [1, 2, 3, 4, 5]
+lista2 = [1, 2, 'Vinicius', 'Murilo', 3]
+
+#Create
+lista2.append('Allan')
+#Insert no index indicado
+lista2.insert(2, 'bob')
+#Update
+lista2[1] = 5
+#Delete
+lista2.remove('bob')
+
+print(lista1)
+print(30* '*')
+print(lista2)
+
